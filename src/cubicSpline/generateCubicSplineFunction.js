b529e80e758ebc357ac622a100399695ac623e21
James Johnson <johnsojr@umn.edu>
2021-07-28 20:18:48 -0500

    Updates

--- src/cubicSpline/generateCubicSplineFunction.js
+++ src/cubicSpline/generateCubicSplineFunction.js
@@ -7,7 +7,7 @@ export default async (data) => {
   const basisMatrix = new Matrix(xs.map((x) => [1, x, x ** 2, x ** 3]));
   const transposedBasisMatrix = await basisMatrix.transpose();
   const Y = await new Matrix([ys]).transpose();
-  const betas = await transposedBasisMatrix.multiply(basisMatrix).then((M) => M.inverse()).then((M) => M.multiply(Y)).then((M) => M.toArray().flat());
+  const betas = await transposedBasisMatrix.multiply(basisMatrix).then((M) => M.inverse()).then((M) => M.multiply(transposedBasisMatrix)).then((M) => M.multiply(Y)).then((M) => M.toArray().flat());
   return (x) => {
     const basis = [1, x, x ** 2, x ** 3];
     return betas.reduce((acc, beta, i) => acc + beta * basis[i], 0);
